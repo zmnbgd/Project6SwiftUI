5bacda096a3cc485179f51205ede1bfb94705827
Marko Živanović <zivanovic.marko@icloud.com>
2025-02-19 18:13:00 +0100

    Animations

---
+++ Project6SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project6SwiftUI
+//
+//  Created by Marko Zivanovic on 19.2.25..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project6SwiftUI/Project6SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project6SwiftUIApp.swift
+//  Project6SwiftUI
+//
+//  Created by Marko Zivanovic on 19.2.25..
+//
+
+import SwiftUI
+
+@main
+struct Project6SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
